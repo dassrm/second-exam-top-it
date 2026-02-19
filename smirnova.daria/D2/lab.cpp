e4291c12ba87ef2f5d22c27de3c12245179e7406
dassrm <daryasm1rnoff@yandex.ru>
2026-02-19 19:34:54 +0300

    Implement argument parsing with error handling

--- smirnova.daria/D2/lab.cpp
+++ smirnova.daria/D2/lab.cpp
@@ -1,13 +1,20 @@
 #include "lab.hpp"
 
+#include <cstring>
+
 namespace smirnova {
 
 bool parseArgs(int argc, char const * const * argv, ParseResult & result)
 {
   result.reverse = false;
-  (void)argc;
-  (void)argv;
-  return true;
+  if (argc == 1) {
+    return true;
+  }
+  if (argc == 2 && std::strcmp(argv[1], "reverse") == 0) {
+    result.reverse = true;
+    return true;
+  }
+  return false;
 }
 
 void processInput(bool reverse)
--- smirnova.daria/D2/main.cpp
+++ smirnova.daria/D2/main.cpp
@@ -1,9 +1,14 @@
 #include "lab.hpp"
 
+#include <iostream>
+
 int main(int argc, char * argv[])
 {
   smirnova::ParseResult args;
-  smirnova::parseArgs(argc, const_cast<char const * const *>(argv), args);
+  if (!smirnova::parseArgs(argc, const_cast<char const * const *>(argv), args)) {
+    std::cerr << "Usage: ./lab [reverse]\n";
+    return 1;
+  }
   smirnova::processInput(args.reverse);
   return 0;
 }
